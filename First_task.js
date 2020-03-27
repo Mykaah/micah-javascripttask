50dac1f06dfa69e1c694694b9473d310ec919775
Mykaah <57074860+Mykaah@users.noreply.github.com>
2020-03-27 14:58:33 +0100

    Add files via upload

---
+++ First_task.js
@@ -0,0 +1,10 @@
+let name = 'Micah';
+console.log(name);
+var courses = ["HTML", "CSS ", "JavaScript ", "C# ", "Python ", "PHP ", "Go ", "UI/UX"];
+console.log(courses);
+number_of_courses = 8
+for (let n = 1; n <= 200; n++) {
+    if (n % 2 == 0) console.log(n); 
+}
+
+
